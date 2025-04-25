8169a227e71a0993fab52e6aae5180c9c0d16a83
mohamedrady1 <mohamedrady1116@gmail.com>
2025-04-25 14:38:52 +0300

    Refactor code structure for improved readability and maintainability and Add partners page

---
+++ js/partners.js
@@ -0,0 +1,266 @@
+const partnerDataEn = [
+    {
+        id: 1,
+        imgUrl: './images/clients/wrthksa_logo.svg',
+        altText: 'Wrth Platform Logo',
+        title: 'Wrth',
+        description: 'Wrth is a platform dedicated to preserving and enhancing cultural heritage through modern technology. We harness AI to document and artistically represent traditional folklore in innovative ways. Toward a digital future that preserves the authenticity of the past with a modern spirit. We offer tools that help create visual content inspired by heritage. Start your creative journey with Wrth now!'
+    },
+    {
+        id: 2,
+        imgUrl: './images/clients/2.png',
+        altText: 'Human Resources Development Fund Logo',
+        title: 'Human Resources Development Fund',
+        description: 'We believe in the importance of developing national skills and empowering talents. We provide AI tools that support training and career development. Through interactive visuals, we highlight professional growth opportunities. Help us build a bright career future using advanced technologies. Learn, create, and grow with the Human Resources Development Fund.'
+    },
+    {
+        id: 3,
+        imgUrl: './images/clients/17.png',
+        altText: 'Ministry of Municipal and Rural Affairs and Housing Logo',
+        title: 'Ministry of Municipal and Rural Affairs and Housing',
+        description: 'A modern vision for developing cities and communities using technology. We integrate AI into urban planning and design. Create innovative urban layouts that reflect your ideal city. Toward smart and sustainable cities that meet the aspirations of future generations. Start reimagining infrastructure with AI tools.'
+    },
+    {
+        id: 4,
+        imgUrl: './images/clients/16.webp',
+        altText: 'Kafa’a Center Logo',
+        title: 'Kafa’a',
+        description: 'We help you achieve the highest levels of efficiency in work and production. Using advanced AI, we offer visual solutions that reflect quality and excellence. Design content that enhances your projects’ efficiency and attracts your target audience. Make continuous improvement a part of your professional journey. With Kafa’a, development is not an option—it’s a way of life.'
+    },
+    {
+        id: 5,
+        imgUrl: './images/clients/05.png',
+        altText: 'Saudi Standards, Metrology and Quality Organization Logo',
+        title: 'SASO',
+        description: 'Precise standards and high quality presented through modern technologies. Use AI to create visuals that reflect adherence to standards. We provide tools that visually express the quality of products and services. Comprehensive support for meeting standards in a creative and artistic way. Start designing with trust and professionalism with SASO.'
+    },
+    {
+        id: 6,
+        imgUrl: './images/clients/07.webp',
+        altText: 'National Company for Mechanical Systems Logo',
+        title: 'National Company for Mechanical Systems',
+        description: 'A leader in developing smart and advanced mechanical systems. Create technical visuals that demonstrate the complexity and precision of our products. AI enables you to visually represent industrial ideas. Visual solutions supported by advanced technologies to showcase engineering innovation. Elevate your technical presentation with us.'
+    },
+    {
+        id: 7,
+        imgUrl: './images/clients/4.svg',
+        altText: 'Mazaree Electronic Platform Logo',
+        title: 'Mazaree Platform',
+        description: 'An electronic platform aiming to provide an innovative digital environment for farmers. It enables them to manage their farms, sell their products, and access online agricultural consultations. Our goal is to enhance agricultural production through modern technologies.'
+    },
+    {
+        id: 8,
+        imgUrl: './images/clients/006.png',
+        altText: 'Weqaa Center Logo',
+        title: 'Weqaa Center',
+        description: 'We believe in the importance of developing national skills and empowering talents. We provide AI tools that support training and career development. Through interactive visuals, we highlight professional growth opportunities. Help us build a bright career future using advanced technologies.'
+    },
+    {
+        id: 9,
+        imgUrl: './images/clients/8.webp',
+        altText: 'MODON Logo',
+        title: 'MODON',
+        description: 'A modern vision for developing cities and communities using technology. We integrate AI into urban planning and design. Create innovative urban layouts that reflect your ideal city. Toward smart and sustainable cities that meet the aspirations of future generations.'
+    },
+    {
+        id: 10,
+        imgUrl: './images/clients/10.svg',
+        altText: 'Saudi Water Authority Logo',
+        title: 'Saudi Water Authority',
+        description: 'We help you achieve the highest levels of efficiency in work and production. Using advanced AI, we offer visual solutions that reflect quality and excellence. Design content that enhances your projects’ efficiency and attracts your target audience. With Kafa’a, development is not an option—it’s a way of life.'
+    },
+    {
+        id: 11,
+        imgUrl: './images/clients/11.png',
+        altText: 'Ministry of Islamic Affairs Logo',
+        title: 'Ministry of Islamic Affairs',
+        description: 'Precise standards and high quality presented through modern technologies. Use AI to create visuals that reflect adherence to standards. We provide tools that visually express the quality of products and services. Comprehensive support for meeting standards in a creative and artistic way.'
+    },
+    {
+        id: 12,
+        imgUrl: './images/clients/12.svg',
+        altText: 'National Company for Mechanical Systems Logo',
+        title: 'National Company for Mechanical Systems',
+        description: 'A leader in developing smart and advanced mechanical systems. Create technical visuals that demonstrate the complexity and precision of our products. AI enables you to visually represent industrial ideas. Visual solutions supported by advanced technologies to showcase engineering innovation.'
+    },
+    {
+        id: 13,
+        imgUrl: './images/clients/0014.png',
+        altText: 'Institute of Consulting and Business Solutions Logo',
+        title: 'Institute of Consulting and Business Solutions',
+        description: 'We offer innovative solutions in business consulting and management. The institute relies on AI to improve management operations and data analysis. It helps develop business strategies and achieve sustainable growth using advanced technologies.'
+    },
+    {
+        id: 14,
+        imgUrl: './images/clients/015.png',
+        altText: 'National Center for e-Learning Logo',
+        title: 'National Center for e-Learning',
+        description: 'The center aims to enhance e-learning by offering innovative solutions for teachers and students. It provides modern technologies to create flexible and effective learning environments, using AI to personalize educational content and boost learning efficiency.'
+    }
+];
+const partnerDataAr = [
+    {
+        id: 1,
+        imgUrl: './images/clients/wrthksa_logo.svg',
+        altText: 'شعار منصة ورث',
+        title: 'ورث',
+        description: 'منصة "ورث" تسعى للحفاظ على التراث الثقافي وتعزيزه عبر الوسائل التقنية الحديثة. نُسخّر الذكاء الاصطناعي في توثيق وتجميل الموروث الشعبي بطريقة مبتكرة. نحو مستقبل رقمي يحافظ على أصالة الماضي بروح عصرية. نوفر أدوات تساعد على تصميم محتوى بصري مستوحى من التراث. ابدأ رحلتك الإبداعية مع "ورث" الآن!'
+    },
+    {
+        id: 2,
+        imgUrl: './images/clients/2.png',
+        altText: 'شعار صندوق تنمية الموارد البشرية',
+        title: 'صندوق تنمية الموارد البشرية',
+        description: 'نؤمن بأهمية تطوير المهارات الوطنية وتمكين الكفاءات. نوفر أدوات ذكاء اصطناعي تدعم التدريب والتأهيل الوظيفي. من خلال الصور التفاعلية، نسلّط الضوء على فرص التطوير المهني. ساعدنا في بناء مستقبل وظيفي مشرق باستخدام تقنيات متقدمة. تعلم، أنشئ، وتطور مع صندوق تنمية الموارد البشرية.'
+    },
+    {
+        id: 3,
+        imgUrl: './images/clients/17.png',
+        altText: 'شعار وزارة الشؤون البلدية والإسكان',
+        title: 'وزارة البلديات والاسكان',
+        description: 'رؤية حديثة لتطوير المدن والمجتمعات باستخدام التكنولوجيا. ندمج الذكاء الاصطناعي في التخطيط العمراني والتصميم الحضري. أنشئ تصاميم عمرانية مبتكرة تعبّر عن مدينتك المثالية. نحو مدن ذكية مستدامة تحاكي طموحات الأجيال القادمة. ابدأ بإعادة تخيل البنية التحتية مع أدوات الذكاء الاصطناعي.'
+    },
+    {
+        id: 4,
+        imgUrl: './images/clients/16.webp',
+        altText: 'شعار مركز كفاءة',
+        title: 'كفاءة',
+        description: 'نساعدك في الوصول إلى أعلى مستويات الكفاءة في العمل والإنتاج. بذكاء اصطناعي متطور، نوفر حلولًا بصرية تعكس الجودة والتميز. صمم محتوى يعزز من كفاءة مشاريعك ويجذب جمهورك المستهدف. اجعل التحسين المستمر جزءًا من رحلتك المهنية. مع "كفاءة"، التطوير ليس خيارًا بل أسلوب حياة.'
+    },
+    {
+        id: 5,
+        imgUrl: './images/clients/05.png',
+        altText: 'شعار الهيئة السعودية للمواصفات والمقاييس',
+        title: 'المواصفات السعودية',
+        description: 'معايير دقيقة وجودة عالية نقدمها بتقنيات عصرية. استخدم الذكاء الاصطناعي لإنشاء صور تعكس الالتزام بالمواصفات. نوفّر أدوات تساعد في التعبير البصري عن جودة المنتجات والخدمات. دعم شامل لمطابقة المعايير بأسلوب إبداعي وفني. ابدأ بتصميم يعكس الثقة والاحترافية مع المواصفات السعودية.'
+    },
+    {
+        id: 6,
+        imgUrl: './images/clients/07.webp',
+        altText: 'شعار الشركة الوطنية للأنظمة الميكانيكية',
+        title: 'الشركة الوطنية للأنظمة الميكانيكية',
+        description: 'ريادة في تطوير الأنظمة الميكانيكية الذكية والمتقدمة. أنشئ صورًا تقنية توضح تعقيد ودقة منتجاتنا. الذكاء الاصطناعي يمكّنك من تجسيد الأفكار الصناعية بصريًا. حلول بصرية مدعومة بتقنيات متقدمة لتوضيح الابتكار الهندسي. ارفع مستوى عرضك التقني مع الشركة الوطنية للأنظمة الميكانيكية.'
+    },{
+        id: 7,
+        imgUrl: './images/clients/4.svg',
+        altText: 'شعار بوابة مزارع الإلكترونية',
+        title: 'بوابة مزارع الإلكترونية',
+        description: 'منصة إلكترونية تهدف إلى توفير بيئة رقمية مبتكرة للمزارعين، حيث تمكنهم من إدارة مزارعهم، بيع منتجاتهم الزراعية، والحصول على استشارات زراعية عبر الإنترنت. نهدف إلى تحسين الإنتاج الزراعي باستخدام تقنيات حديثة.'
+    },
+    {
+        id: 2,
+        imgUrl: './images/clients/006.png',
+        altText: 'شعار مركز وقاء',
+        title: 'مركز وقاء',
+        description: 'نؤمن بأهمية تطوير المهارات الوطنية وتمكين الكفاءات. نوفر أدوات ذكاء اصطناعي تدعم التدريب والتأهيل الوظيفي. من خلال الصور التفاعلية، نسلّط الضوء على فرص التطوير المهني. ساعدنا في بناء مستقبل وظيفي مشرق باستخدام تقنيات متقدمة.'
+    },
+    {
+        id: 3,
+        imgUrl: './images/clients/8.webp',
+        altText: 'شعار مدن',
+        title: 'مدن',
+        description: 'رؤية حديثة لتطوير المدن والمجتمعات باستخدام التكنولوجيا. ندمج الذكاء الاصطناعي في التخطيط العمراني والتصميم الحضري. أنشئ تصاميم عمرانية مبتكرة تعبّر عن مدينتك المثالية. نحو مدن ذكية مستدامة تحاكي طموحات الأجيال القادمة.'
+    },
+    {
+        id: 4,
+        imgUrl: './images/clients/10.svg',
+        altText: 'شعار الهيئة السعودية للمياه',
+        title: 'الهيئة السعودية للمياه',
+        description: 'نساعدك في الوصول إلى أعلى مستويات الكفاءة في العمل والإنتاج. بذكاء اصطناعي متطور، نوفر حلولًا بصرية تعكس الجودة والتميز. صمم محتوى يعزز من كفاءة مشاريعك ويجذب جمهورك المستهدف. مع "كفاءة"، التطوير ليس خيارًا بل أسلوب حياة.'
+    },
+    {
+        id: 5,
+        imgUrl: './images/clients/11.png',
+        altText: 'شعار وزارة الشؤون الإسلامية والدعوة والإرشاد',
+        title: 'وزارة الشؤون الإسلامية والدعوة والإرشاد',
+        description: 'معايير دقيقة وجودة عالية نقدمها بتقنيات عصرية. استخدم الذكاء الاصطناعي لإنشاء صور تعكس الالتزام بالمواصفات. نوفّر أدوات تساعد في التعبير البصري عن جودة المنتجات والخدمات. دعم شامل لمطابقة المعايير بأسلوب إبداعي وفني.'
+    },
+    {
+        id: 6,
+        imgUrl: './images/clients/12.svg',
+        altText: 'شعار الشركة الوطنية للأنظمة الميكانيكية',
+        title: 'الشركة الوطنية للأنظمة الميكانيكية',
+        description: 'ريادة في تطوير الأنظمة الميكانيكية الذكية والمتقدمة. أنشئ صورًا تقنية توضح تعقيد ودقة منتجاتنا. الذكاء الاصطناعي يمكّنك من تجسيد الأفكار الصناعية بصريًا. حلول بصرية مدعومة بتقنيات متقدمة لتوضيح الابتكار الهندسي.'
+    },
+    {
+        id: 6,
+        imgUrl: './images/clients/0014.png',
+        altText: 'شعار معهد الاستشارات وحلول الأعمال',
+        title: 'معهد الاستشارات وحلول الأعمال',
+        description: 'نقدم حلولًا مبتكرة في مجال الاستشارات وإدارة الأعمال. يعتمد المعهد على الذكاء الاصطناعي لتحسين عمليات الإدارة وتحليل البيانات. يساعد في تطوير استراتيجيات الأعمال وتحقيق النمو المستدام باستخدام تقنيات متقدمة.'
+    },
+    {
+        id: 6,
+        imgUrl: './images/clients/015.png',
+        altText: 'شعار المركز الوطني للتعليم الإلكتروني',
+        title: 'المركز الوطني للتعليم الإلكتروني',
+        description: 'يسعى المركز إلى تعزيز التعليم الإلكتروني عبر تقديم حلول مبتكرة للمعلمين والطلاب. يوفر تقنيات حديثة لخلق بيئة تعليمية مرنة وفعّالة، باستخدام الذكاء الاصطناعي لتخصيص المحتوى التعليمي ورفع كفاءة العملية التعليمية.'
+    }
+]
+
+var currentURL = window.location.href;
+const partnerData = currentURL.includes('-ar') ? partnerDataAr : partnerDataEn;
+
+function displayPartners() {
+    const partnerList = document.getElementById('partners-list');
+    console.log(partnerList);
+    partnerList.innerHTML = '';
+
+    partnerData.forEach(partner => {
+        const li = document.createElement('li');
+        li.classList.add('partner-item', 'col-md-6', 'col-lg-3', 'mb-4');
+        li.innerHTML = `
+        <article>
+            <div class="partner-img d-flex align-items-center justify-content-center w-100 mb-3">
+                <img src="${partner.imgUrl}" alt="${partner.altText}" class="w-100 h-100">
+            </div>
+            <div class="d-flex justify-content-between flex-column gap-2 text-md-start text-center">
+                <div>
+                    <h2 class="fs-5-2 lh-sm text-dark partner-title">
+                        ${partner.title}
+                    </h2>
+                    <p class="fs-6-1 ls-1 text-black-50 mt-2 fw-lighter partner-description mb-0">
+                        ${partner.description}
+                    </p>
+                </div>
+                <!-- سيتم إخفاء الزر إذا كان النص صغيرًا -->
+                <div class="d-flex justify-content-end w-100">
+                    <button class="read-more-btn" style="display: ${partner.description.length > 100 ? 'block' : 'none'};">
+                        <svg viewBox="0 0 24 24" width="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path transform="rotate(180 12 12)" d="M18.2929 15.2893C18.6834 14.8988 18.6834 14.2656 18.2929 13.8751L13.4007 8.98766C12.6195 8.20726 11.3537 8.20757 10.5729 8.98835L5.68257 13.8787C5.29205 14.2692 5.29205 14.9024 5.68257 15.2929C6.0731 15.6835 6.70626 15.6835 7.09679 15.2929L11.2824 11.1073C11.673 10.7168 12.3061 10.7168 12.6966 11.1073L16.8787 15.2893C17.2692 15.6798 17.9024 15.6798 18.2929 15.2893Z" fill="#0F0F0F"></path>
+                        </svg>
+                        ${currentURL.includes('-ar') ? 'اقرأ المزيد' : 'Read more'}
+                    </button>
+                </div>
+            </div>
+        </article>
+        `;
+    
+        const button = li.querySelector('.read-more-btn');
+        const description = li.querySelector('.partner-description');
+    
+        button.addEventListener('click', () => {
+            const isExpanded = description.classList.toggle('expanded');
+            if(isExpanded){
+                button.classList.add('expanded');
+            }
+            button.innerHTML = `
+                <svg viewBox="0 0 24 24" width="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path transform="${isExpanded ? 'rotate(0 12 12)' : 'rotate(180 12 12)'}" 
+                        d="M18.2929 15.2893C18.6834 14.8988 18.6834 14.2656 18.2929 13.8751L13.4007 8.98766C12.6195 8.20726 11.3537 8.20757 10.5729 8.98835L5.68257 13.8787C5.29205 14.2692 5.29205 14.9024 5.68257 15.2929C6.0731 15.6835 6.70626 15.6835 7.09679 15.2929L11.2824 11.1073C11.673 10.7168 12.3061 10.7168 12.6966 11.1073L16.8787 15.2893C17.2692 15.6798 17.9024 15.6798 18.2929 15.2893Z" fill="#0F0F0F"></path>
+                </svg>
+                ${currentURL.includes('-ar') 
+                    ? (isExpanded ? 'إقرأ أقل' : 'اقرأ المزيد') 
+                    : (isExpanded ? 'Read less' : 'Read more')}
+            `;
+        });
+    
+        partnerList.appendChild(li);
+    });
+}
+
+
+function init() {
+    displayPartners();
+}
+init();
